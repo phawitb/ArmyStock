4e96e3834b5376547e34ac067d51a3c4931777cd
phawitb <96680592+phawitb@users.noreply.github.com>
2024-10-01 22:46:30 +0700

    Add files via upload

--- run_streamlit.sh
+++ run_streamlit.sh
@@ -6,6 +6,10 @@ source /home/phawit/anaconda3/etc/profile.d/conda.sh  # Adjust this path if nece
 conda activate rifle  # Replace 'your_env_name' with your actual environment name
 cd /home/phawit/Documents/ArmyStock
 
+python load_data_from_googlesheet.py
+
+sleep 3
+
 streamlit run app.py --server.headless true --server.port 8509 &
 
 sleep 5
